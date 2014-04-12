5e5f52ec71e3874a85c92a12d82ce528e59f5c3d
Michael Mattax <michael@formstack.com>
2014-04-11 22:37:32 -0400

    initial commit

---
+++ gearman_backlog/gearman_backlog.rb
@@ -0,0 +1,20 @@
+class GearmanBacklog < Scout::Plugin
+  require 'net/telnet'
+  OPTIONS=<<-EOS
+    host:
+      name: Host
+      default: localhost
+      notes: Hostname or IP address of the Gearman job server
+    port:
+      name Port
+      default: 4730
+  EOS
+  def build_report
+    telnet = Net::Telnet::new("Host" => option(:host), "Port" => option(:port))
+    status = telnet.cmd("String" => "status", "Match" => /\n./)
+    status.split("\n")[0...-1].map do |job|
+      name, total, running, workers = job.split("\t")
+      report(name => total)
+    end
+  end
+end
